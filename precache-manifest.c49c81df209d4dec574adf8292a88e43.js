616dbe5794d61c1f7a28e8894e2185e21e0dd1cb
AleksandrKoltsov <41367097+AleksandrKoltsov@users.noreply.github.com>
2020-03-13 17:40:43 +0200

    Updates

---
+++ precache-manifest.c49c81df209d4dec574adf8292a88e43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ddc05859500060586eca8090fc82a56",
+    "url": "/githubvacancy/index.html"
+  },
+  {
+    "revision": "195e22154e985d6a02fa",
+    "url": "/githubvacancy/static/css/main.a8d4631e.chunk.css"
+  },
+  {
+    "revision": "e74a5737ff5b290f6493",
+    "url": "/githubvacancy/static/js/2.c14799f8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/githubvacancy/static/js/2.c14799f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "195e22154e985d6a02fa",
+    "url": "/githubvacancy/static/js/main.34d67574.chunk.js"
+  },
+  {
+    "revision": "71978067e36f5595c429",
+    "url": "/githubvacancy/static/js/runtime-main.1ba535f1.js"
+  }
+]);
